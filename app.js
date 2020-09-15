12b96e3b42eb42b847ffe8f922abae5270d1d884
phametic <ram.pham@gmail.com>
2020-09-15 00:03:40 -0400

    Add Button Finished
    Finished adding in add button. Adds in the select game from the form. Reset form after clicking add. Completed status changes color based on complete or not complete.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //Create video game object
-function Game(title, console, genre, score, completed) {
+function Game(title, console, genre, rating, score, completed) {
   this.title = title;
   this.console = console;
   this.genre = genre;
+  this.rating = rating;
   this.score = score;
   this.completed = completed;
 }
@@ -58,6 +59,7 @@ function render(arr) {
     const gameTitle = document.createElement("p");
     const gameConsole = document.createElement("p");
     const gameGenre = document.createElement("p");
+    const gameRating = document.createElement("p");
     const gameScore = document.createElement("p");
     const gameCompleted = document.createElement("p");
     const container = document.createElement("div");
@@ -74,12 +76,16 @@ function render(arr) {
     gameGenre.className = "GameGenreStyle";
     gameGenre.textContent = "Genre: " + element.genre;
 
+    gameRating.className = "GameRatingStyle";
+    gameRating.textContent = "ESRB Rating: " + element.rating;
+
     gameScore.className = "GameScoreStyle";
     gameScore.textContent = "Review Score: " + element.score;
 
     gameCompleted.className = "GameCompletedStyle";
     gameCompleted.id = "gameCompleted";
-    gameCompleted.textContent = element.played ? "Completed." : "Not Completed.";
+    gameCompleted.textContent = element.completed ? "Completed" : "Not Completed";
+    gameCompleted.style.color = element.completed ? "#25FC0D" : "white";
 
     optionsPanel.className = "OptionsPanel";
 
@@ -92,6 +98,7 @@ function render(arr) {
     container.appendChild(gameTitle);
     container.appendChild(gameConsole);
     container.appendChild(gameGenre);
+    container.appendChild(gameRating);
     container.appendChild(gameScore);
 
     optionsPanel.appendChild(gameCompleted);
@@ -109,17 +116,42 @@ function loadListeners() {
   const gameCompletedText = container.querySelector("#gameCompleted");
   const containerId = container.getAttribute('id').slice(10);
   const deleteBtn = container.querySelector(".deleteButton");
-
+ 
   gameCompletedText.addEventListener("click", element => toggleGameCompletion(element, containerId));
   deleteBtn.addEventListener("click", element => deleteGame(containerId));
+ 
  });
+
   
 }
 
 //add game to array
-function addGame() {
-   
+function addGame(title, console, genre, rating, score, gameStatus) {
+  const newGame = new Game(title, console, genre, rating, score, gameStatus);
+  
+  myLibrary.push(newGame);
+  render(myLibrary);
 }
+
+let gameTitle = document.getElementById("gameTitle");
+let gameConsole = document.getElementById("gameConsole");
+let gameGenre = document.getElementById("gameGenre");
+let gameRating = document.getElementById("gameRating");
+let gameScore = document.getElementById("gameScore");
+let gameStatus = document.getElementById("gameCompleted");
+
+document.getElementById("inputGameBtn").onclick = () => {
+  alert("Game Added!");
+  addGame(gameTitle.value,gameConsole.value,gameGenre.value, gameRating.value,gameScore.value,gameStatus.checked);
+  modal.style.display = "none";
+  gameTitle.value = "";
+  gameConsole.value = "";
+  gameGenre.value = "";
+  gameRating.value = "";
+  gameScore.value = "";
+  gameStatus.checked = false;
+  loadListeners();
+};
 
 //Delete from array
 function deleteGame(containerId) {
